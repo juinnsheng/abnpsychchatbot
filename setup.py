88209adf2f474d6a12a0f12257420165f9076a92
juinnsheng <116355442+juinnsheng@users.noreply.github.com>
2026-01-01 21:10:26 +0800

    requirements

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="abnormalpsych_chatbot",
+    version="0.1.0",
+    author="Jensen",
+    author_email="jensenforcode23@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
